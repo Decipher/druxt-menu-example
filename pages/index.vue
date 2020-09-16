a9e4485a5b21a86f3f4716dfb43f3aca872ade53
Stuart Clark <stuart@realityloop.com>
2020-09-16 17:17:35 +1000

    Updated example for subtree.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
-    <druxt-menu name="main" />
+    <druxt-menu component="ul" name="main">
+      <template #item="{ item: { entity }, to }">
+        <li>
+          <nuxt-link :to="to">{{ entity.attributes.title }}</nuxt-link>
+        </li>
+      </template>
+
+      <template #parent="{ item: { entity, children }, to }">
+        <li>
+          <nuxt-link :to="to">{{ entity.attributes.title }}</nuxt-link>
+          <ul>
+            <druxt-menu-item
+              v-for="item in children"
+              :key="item.entity.id"
+              :item="item"
+            />
+          </ul>
+        </li>
+      </template>
+    </druxt-menu>
   </div>
 </template>
